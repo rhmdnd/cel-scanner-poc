715aab5f056a64f594664a82b7fad36173fcbb41
Lance Bragstad <lbragsta@redhat.com>
2024-07-31 15:08:20 -0500

    Parse basic yaml files
    Given something that looks like a Rule Custom Resource Defintion,
    convert it into a struct from a YAML file so we can start experimenting
    with resource collection and scanning.

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+
+	yaml "sigs.k8s.io/yaml/goyaml.v3"
+)
+
+var ruleFile string
+
+type rule struct {
+	Kind       string   `yaml:"kind"`
+	CheckType  string   `yaml:"checkType"`
+	Title      string   `yaml:"title"`
+	Expression string   `yaml:"expression"`
+	Inputs     []string `yaml:"inputs"`
+}
+
 func main() {
-	// read rule as input
+	validate()
+
+	read(ruleFile)
 
 	// collect dependencies (e.g., get file metadata information or
 	// Kubernetes resource information)
@@ -11,4 +31,30 @@ func main() {
 	// run the CEL program with the inputs
 
 	// report the findings
+}
+
+// Validate inputs
+func validate() {
+	flag.StringVar(&ruleFile, "i", "", "Path to a YAML file containing a rule")
+	flag.Parse()
+
+	if ruleFile == "" {
+		panic("Must use -i to pass in a rule file")
+	}
+}
+
+// Read a file as a given path `p` and return a rule struct that represents the
+// YAML contents.
+func read(p string) *rule {
+	r := rule{}
+
+	f, err := ioutil.ReadFile(p)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to read %s: %s", p, err))
+	}
+	err = yaml.Unmarshal(f, &r)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to parse YAML: %s", err))
+	}
+	return &r
 }
